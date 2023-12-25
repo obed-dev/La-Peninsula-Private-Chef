8cc272ac7f407cb344d6bf8b41d5cb2f3e144220
[Obed Baltodano] <obedbaltodano23@homtail.com>
2023-12-24 18:03:37 -0600

    add email js

---
+++ email.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_wudf80r';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Message';
+      Swal.fire({
+        position: "top",
+        icon: "success",
+        title: "Your message has been sent",
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        
+      });
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function(){
+        let formulario = document.getElementById('form');
+        formulario.addEventListener('submit', function() {
+          formulario.reset();
+        });
+      });
+});
